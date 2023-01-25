fe3f706866d086f944cf2b9f9cf7594803165263
munxar <sascha.aeppli@liip.ch>
2023-01-25 13:58:43 +0100

    Update +page.server.ts
    typo

--- frontend/src/routes/about/+page.server.ts
+++ frontend/src/routes/about/+page.server.ts
@@ -1,6 +1,6 @@
 // src/routes/about.server.ts
 
-// you can make some calls to an api, or load markdown from the filesystem to build a documentation site or the lile. 
+// you can make some calls to an api, or load markdown from the filesystem to build a documentation site or the like. 
 // this will happen at build time, so don't call your own go api!
 export async function load() {
 	return {
