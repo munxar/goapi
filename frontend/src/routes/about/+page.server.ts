9187a7ce3c8146361773298b2dc14d3f217647e1
munxar <sascha.aeppli@liip.ch>
2023-01-25 13:56:12 +0100

    Update +page.server.ts
    add description for serve load case.

--- frontend/src/routes/about/+page.server.ts
+++ frontend/src/routes/about/+page.server.ts
@@ -1,3 +1,7 @@
+// src/routes/about.server.ts
+
+// you can make some calls to an api, or load markdown from the filesystem to build a documentation site or the lile. 
+// this will happen at build time, so don't call your own go api!
 export async function load() {
 	return {
 		message: 'Hello from the server at build time'
